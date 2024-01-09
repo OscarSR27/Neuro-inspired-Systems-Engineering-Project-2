54a8d1cb94c21213427327266af2bab6e79cc093
Alexandra <alexandra.samoylova@tum.de>
2024-01-09 14:20:03 +0100

    Change in x axis labelling in plotting code file, creation of task 1 folder

--- tutorial-1/task_5_plotting.py
+++ tutorial-1/task_5_plotting.py
@@ -15,9 +15,9 @@ data.drop(data.columns[-1], axis=1, inplace=True)
 fig, axes = plt.subplots(nrows=len(data.columns), ncols=1, figsize=(20, 10), sharex=True)
 
 for i, column in enumerate(data.columns):
-    axes[i].plot(data.index, data[column], label=column)
+    axes[i].plot(data.index*0.01, data[column], label=column)
     axes[i].set_ylabel(column)
-axes[-1].set_xlabel('Time')
+axes[-1].set_xlabel('Time [s]')
 plt.tight_layout()
 
 # Show the plot
