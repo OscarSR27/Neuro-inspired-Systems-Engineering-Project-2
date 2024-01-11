9f27f47c9dc2bff569f62ad37e190f914eaac33e
Kunal Aggarwal <kunal.aggarwal@tum.de>
2024-01-11 17:57:56 +0100

    task 4 finished

---
+++ tutorial-1/task_4/task_4_plotting.py
@@ -0,0 +1,24 @@
+#%% 
+# imports
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#%%
+# load data
+data = pd.read_csv(r'task_4_four_neurons/task_4_four_neurons_data.txt')
+
+#remove empty column
+data.drop(data.columns[-1], axis=1, inplace=True)
+
+#%%
+# plotting
+fig, axes = plt.subplots(nrows=len(data.columns), ncols=1, figsize=(20, 10), sharex=True)
+
+for i, column in enumerate(data.columns):
+    axes[i].plot(data.index*0.01, data[column], label=column)
+    axes[i].set_ylabel(column)
+axes[-1].set_xlabel('Time [s]')
+plt.tight_layout()
+
+# Show the plot
+plt.show()
