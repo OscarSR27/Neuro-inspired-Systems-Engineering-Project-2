790dc9ad49084a349dca294e9339931d4b053997
Katja Frey <katja.frey@tum.de>
2024-01-28 16:31:43 +0100

    updated plotting of Matsuoka neurons

--- tutorial-1/task_5/task_5_plotting.py
+++ tutorial-1/task_5/task_5_plotting.py
@@ -7,7 +7,7 @@ import os
 
 #%%
 # load data
-filename = 'task_5_data_eight_coupling.txt'
+filename = 'task_5_data_eight_coupling_no-perturbation.txt'
 data = pd.read_csv(filename)
 
 # extract file name without file extension
@@ -17,19 +17,36 @@ description, _ = os.path.splitext(filename)
 data.drop(data.columns[-1], axis=1, inplace=True)
 
 #%%
-# plotting
+# plotting all neurons
 n_rows = math.ceil(len(data.columns)/2)
 fig, axes = plt.subplots(nrows=n_rows, ncols=2, figsize=(20, 10), sharex=True)
 
-axes = axes.flatten()
+#axes = axes.flatten()
 
 for i, column in enumerate(data.columns):
-    axes[i].plot(data.index*0.01, data[column], label=column)
-    axes[i].set_ylabel(column)
-axes[-2].set_xlabel('Time [s]')
-axes[-1].set_xlabel('Time [s]')
+    if i < n_rows:
+        axes[i][0].plot(data.index*0.01, data[column], label=column)
+        axes[i][0].set_ylabel(column)
+    else:
+        axes[i-n_rows][1].plot(data.index*0.01, data[column], label=column)
+        axes[i-n_rows][1].set_ylabel(column)
+axes[n_rows-1][0].set_xlabel('Time [s]')
+axes[n_rows-1][1].set_xlabel('Time [s]')
 plt.tight_layout()
 plt.savefig(f'{description}.png')
 
 # Show the plot
+plt.show()
+
+#%%
+# plotting subtraction of horizontally aligned neurons
+n_rows = math.ceil(len(data.columns)/2)
+fig, axes = plt.subplots(nrows=n_rows, figsize=(20, 10), sharex=True)
+
+for i in range(n_rows):
+    osc = data.iloc[:,i] - data.iloc[:,i+n_rows]
+    axes[i]. plot(data.index, osc)
+    axes[i].set_ylabel('Motor '+str(i+1))
+axes[-1].set_xlabel('Time')
+plt.tight_layout()
 plt.show()
